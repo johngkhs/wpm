678911115db3737e295945d7591705de0a269256
John Kaczor <johngk@google.com>
2019-10-11 14:33:01 -0700

    Minor cleanups.

--- wpm.py
+++ wpm.py
@@ -14,7 +14,18 @@ import textwrap
 import time
 
 
+CHARS_PER_WORD = 5.0
+CURSOR_NOT_VISIBLE = 0
+CURSOR_VERY_VISIBLE = 2
+DEFAULT_LOCALE = ''
 ENCODING = 'utf8'
+ERROR_COLOR_ID = 1
+FIFTY_MILLIS = 50
+GETCH_KEY_DELETE = 127
+GETCH_NO_INPUT = -1
+INTRO_STR = 'Press any key to begin or Ctrl-C to exit'
+OUTRO_STR = 'Press Ctrl-C to exit'
+SECONDS_PER_MINUTE = 60.0
 
 
 class ExitSuccess(Exception):
@@ -51,12 +62,10 @@ def calculate_row_column(wrapped_terminal_lines, index):
 
 
 def minutes_elapsed(seconds_elapsed):
-    SECONDS_PER_MINUTE = 60.0
     return (seconds_elapsed / SECONDS_PER_MINUTE)
 
 
 def calculate_gross_wpm(num_correct_chars_typed, seconds_elapsed):
-    CHARS_PER_WORD = 5.0
     words_typed = (num_correct_chars_typed / CHARS_PER_WORD)
     return int(words_typed / minutes_elapsed(seconds_elapsed))
 
@@ -79,7 +88,7 @@ def wrap_terminal_lines(input_str, term_dims):
     return (wrapped_terminal_lines, wrapped_total_length)
 
 
-def draw_screen(screen, term_dims, input_str, color_id, colored_indexes, footer_str, cursor_index=None):
+def draw_screen(screen, term_dims, input_str, error_char_indices, footer_str, cursor_index=None):
     screen.move(0, 0)
     screen.erase()
     wrapped_terminal_lines, wrapped_total_length = wrap_terminal_lines(input_str, term_dims)
@@ -87,10 +96,10 @@ def draw_screen(screen, term_dims, input_str, color_id, colored_indexes, footer_
       screen.addstr(row, 0, wrapped_line.encode(ENCODING))
     screen.move(term_dims.rows, 0)
     screen.addstr(footer_str[:term_dims.columns - 1].encode(ENCODING))
-    for colored_index in colored_indexes:
-        if colored_index < wrapped_total_length:
-            row, column = calculate_row_column(wrapped_terminal_lines, colored_index)
-            screen.addstr(row, column, input_str[colored_index].encode(ENCODING), curses.color_pair(color_id))
+    for error_char_index in error_char_indices:
+        if error_char_index < wrapped_total_length:
+            row, column = calculate_row_column(wrapped_terminal_lines, error_char_index)
+            screen.addstr(row, column, input_str[error_char_index].encode(ENCODING), curses.color_pair(ERROR_COLOR_ID))
     if cursor_index is not None:
         row, column = calculate_row_column(wrapped_terminal_lines, cursor_index)
         screen.move(row, column)
@@ -100,60 +109,46 @@ def draw_screen(screen, term_dims, input_str, color_id, colored_indexes, footer_
 def is_typing_test_finished(input_str, term_dims, next_char_index, seconds_elapsed):
     wrapped_terminal_lines, wrapped_total_length = wrap_terminal_lines(input_str, term_dims)
     end_char_index = min(wrapped_total_length, len(input_str))
-    if seconds_elapsed >= 60:
-        return True
-    elif next_char_index >= end_char_index:
-        return True
-    else:
-        return False
+    return seconds_elapsed >= 60 or next_char_index >= end_char_index
 
 
 def run_curses(screen, input_str):
     curses.start_color()
     curses.use_default_colors()
-    VERY_VISIBLE = 2
-    curses.curs_set(VERY_VISIBLE)
-    ERROR_COLOR_ID = 1
+    curses.curs_set(CURSOR_VERY_VISIBLE)
     curses.init_pair(ERROR_COLOR_ID, curses.COLOR_BLACK, curses.COLOR_RED)
-    locale.setlocale(locale.LC_ALL, '')
+    locale.setlocale(locale.LC_ALL, DEFAULT_LOCALE)
     term_dims = lookup_terminal_dimensions(screen)
-    incorrect_char_indexes = []
-    next_char_index = 0
-    START_STR = 'Press any key to begin or Ctrl-C to exit'
-    draw_screen(screen, term_dims, input_str, ERROR_COLOR_ID, incorrect_char_indexes, START_STR, cursor_index=next_char_index)
+    next_char_index, error_char_indices = 0, []
+    draw_screen(screen, term_dims, input_str, error_char_indices, INTRO_STR, cursor_index=next_char_index)
     user_input = screen.getch()
     start_time_seconds = time.time()
-    FIFTY_MILLIS = 50
     screen.timeout(FIFTY_MILLIS)
-    NO_INPUT = -1
-    KEY_DELETE = 127
     while True:
-        if user_input == NO_INPUT or user_input == curses.KEY_RESIZE:
+        if user_input == GETCH_NO_INPUT or user_input == curses.KEY_RESIZE:
             pass
-        elif user_input == KEY_DELETE or user_input == curses.KEY_BACKSPACE:
+        elif user_input == GETCH_KEY_DELETE or user_input == curses.KEY_BACKSPACE:
             prev_char_index = max(0, next_char_index - 1)
-            if prev_char_index in incorrect_char_indexes:
-                incorrect_char_indexes.remove(prev_char_index)
+            if prev_char_index in error_char_indices:
+                error_char_indices.remove(prev_char_index)
             next_char_index = prev_char_index
         else:
             correct_next_char = input_str[next_char_index]
             if user_input != ord(correct_next_char):
-                incorrect_char_indexes.append(next_char_index)
+                error_char_indices.append(next_char_index)
             next_char_index += 1
         term_dims = lookup_terminal_dimensions(screen)
         seconds_elapsed = time.time() - start_time_seconds
-        num_incorrect_chars_typed = len(incorrect_char_indexes)
+        num_incorrect_chars_typed = len(error_char_indices)
         num_correct_chars_typed = next_char_index - num_incorrect_chars_typed
         wpm_summary_str = create_wpm_summary_str(num_incorrect_chars_typed, num_correct_chars_typed, seconds_elapsed)
         if is_typing_test_finished(input_str, term_dims, next_char_index, seconds_elapsed):
-            exit_str = 'Press Ctrl-C to exit - {}'.format(wpm_summary_str)
-            NOT_VISIBLE = 0
-            curses.curs_set(NOT_VISIBLE)
+            curses.curs_set(CURSOR_NOT_VISIBLE)
             while True:
                 term_dims = lookup_terminal_dimensions(screen)
-                draw_screen(screen, term_dims, input_str, ERROR_COLOR_ID, incorrect_char_indexes, exit_str)
+                draw_screen(screen, term_dims, input_str, error_char_indices, '{} - {}'.format(OUTRO_STR, wpm_summary_str))
                 user_input = screen.getch()
-        draw_screen(screen, term_dims, input_str, ERROR_COLOR_ID, incorrect_char_indexes, wpm_summary_str, cursor_index=next_char_index)
+        draw_screen(screen, term_dims, input_str, error_char_indices, wpm_summary_str, cursor_index=next_char_index)
         user_input = screen.getch()
 
 
